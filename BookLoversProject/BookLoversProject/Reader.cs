ad0360607857a4c8040c222bb20c94f60704efd7
Cătălina Grămadă <catalina.g1808@gmail.com>
2022-11-08 14:41:37 +0200

    Changed Reader class

--- BookLoversProject/BookLoversProject/Reader.cs
+++ BookLoversProject/BookLoversProject/Reader.cs
@@ -8,18 +8,33 @@ namespace BookLoversProject
 {
     internal class Reader : User
     {
-        public string Name { get; set; }
+        private string firstName;
+        private string lastName;
+        public string Name { 
+            get {
+                return firstName + " " + lastName;
+            }
+            set
+            {
+                int indexOfSpace = value.IndexOf(" ");
+                if (indexOfSpace > 0) {
+                    firstName = value.Substring(0, indexOfSpace);
+                    lastName = value.Substring(indexOfSpace + 1);
+                }
+            }
+        }
         public string ImagePath { get; set; }
         public List<Shelf> BookShelves { get;}
         public List<Reader> Friends { get; set; }
 
-        public Reader(int id, string email, string password, string name, string imagePath, List<Shelf> bookShelves)
+        public Reader(int id, string email, string password, string firstName, string lastName, string imagePath)
          : base(id, email, password)
         {
-            Name = name;
+            this.firstName = firstName;
+            this.lastName = lastName;
             ImagePath = imagePath;
-            BookShelves = bookShelves;
             Friends = new List<Reader>();
+            BookShelves = new List<Shelf>();
         }
 
         //override
